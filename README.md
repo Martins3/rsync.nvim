cdd02d25541434483103281834c7b180179bdb6c
Xueshi Hu <xueshi.hu@smartx.com>
2024-04-06 16:39:26 +0800

    update readme

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # rsync.nvim
 
+A mini version of coffebar/transfer.nvim.
+
+I need a plugin to sync a local project into a virtual machine. Write code locally and test it remotely.
+
+## Status
+
+Work in progress 🔧 .
+
+## Reference
+
 - https://download.samba.org/pub/rsync/rsync.1
 - https://stackoverflow.com/questions/4549945/is-it-possible-to-specify-a-different-ssh-port-when-using-rsync
 
-## TODO
-1. 测试端口
-2. 
+## Similar projects
+
+- [KenN7/vim-arsync](https://github.com/KenN7/vim-arsync)
+- [OscarCreator/rsync.nvim](https://github.com/OscarCreator/rsync.nvim)
+- [coffebar/transfer.nvim](https://github.com/coffebar/transfer.nvim)
--- lua/transfer/transfer.lua
+++ lua/transfer/transfer.lua
@@ -1,64 +1,45 @@
 local M = {}
 
 function M.sync_dir(port, remote_location)
-  local root = vim.fn.system({ "git", "rev-parse", "--show-toplevel" }, vim.fn.expand("%:p"))
-  root = root:sub(1, -2)
+	local root = vim.fn.system({ "git", "rev-parse", "--show-toplevel" }, vim.fn.expand("%:p"))
+	root = root:sub(1, -2)
 
-  local cmd = { "rsync", "--delete", "-avzh", "--exclude=.git/", "--filter=dir-merge,- .gitignore" }
-  if port ~= nil then
-    vim.list_extend(cmd, { "-e", "'ssh -p", port, "'" })
-  end
+	local cmd = "rsync --delete -avzh --exclude=.git/ --filter='dir-merge,- .gitignore' "
+	if port ~= nil then
+		cmd = cmd .. "--rsh='ssh -p" .. port .. "'"
+	end
 
-  vim.list_extend(cmd, { root, remote_location })
-  -- cmd = {"pwd"}
+	cmd = cmd .. " " .. root .. " " .. remote_location
+	-- vim.print(cmd)
 
-  local notification = vim.notify("rsync: " .. remote_location, vim.log.levels.INFO, {
-    title = "Sync started...",
-    icon = " ",
-    timeout = 2000,
-  })
-  local replace
-  if notification ~= nil and notification.Record then
-    replace = notification.Record
-  end
-  local output = {}
-  local stderr = {}
-  vim.fn.jobstart(cmd, {
-    on_stderr = function(_, data, _)
-      if data == nil or #data == 0 then
-        return
-      end
-      vim.list_extend(stderr, data)
-    end,
-    on_stdout = function(_, data, _)
-      for _, line in pairs(data) do
-        if line ~= "" then
-          table.insert(output, line)
-        end
-      end
-    end,
-    on_exit = function(_, code, _)
-      if code ~= 0 then
-        vim.notify(table.concat(stderr, "\n"), vim.log.levels.ERROR, {
-          timeout = 10000,
-          title = "Error running rsync",
-          icon = " ",
-          replace = replace,
-        })
-        return
-      end
+	local output = {}
+	local stderr = {}
+	vim.fn.jobstart(cmd, {
+		on_stderr = function(_, data, _)
+			if data == nil or #data == 0 then
+				return
+			end
+			vim.list_extend(stderr, data)
+		end,
+		on_stdout = function(_, data, _)
+			for _, line in pairs(data) do
+				if line ~= "" then
+					table.insert(output, line)
+				end
+			end
+		end,
+		on_exit = function(_, code, _)
+			if code ~= 0 then
+				vim.notify(table.concat(stderr, "\n"), vim.log.levels.ERROR)
+				return
+			end
 
-      if #output == 0 then
-        output = { "No differences found" }
-      end
-      vim.notify(table.concat(output, "\n"), vim.log.levels.INFO, {
-        timeout = 3000,
-        title = "Sync completed",
-        icon = " ",
-        replace = replace,
-      })
-    end,
-  })
+			if #output == 0 then
+				output = { "No differences found" }
+			end
+			vim.notify(table.concat(output, "\n"), vim.log.levels.INFO)
+		end,
+	})
 end
 
 return M
